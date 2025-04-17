44cef36d00b0ff3f9c51d9a0347073b5fc911e7a
StefanMoCoAt <stefan.mo.co@gmail.com>
2025-04-18 00:38:54 +0200

    init home and upgrade gradle-properties

---
+++ server/src/test/kotlin/at/mocode/ApplicationTest.kt
@@ -0,0 +1,15 @@
+package at.mocode
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class ApplicationTest {
+    @Test
+    fun main() {
+    }
+
+    @Test
+    fun module() {
+    }
+
+}
