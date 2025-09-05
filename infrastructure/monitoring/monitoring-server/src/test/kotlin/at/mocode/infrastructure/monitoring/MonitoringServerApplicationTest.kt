cc7299e25a77b25024039a15b3ad3926066a64c3
stefan <stefan@mo.co.at>
2025-09-05 14:13:13 +0200

    fix tests

--- infrastructure/monitoring/monitoring-server/src/test/kotlin/at/mocode/infrastructure/monitoring/MonitoringServerApplicationTest.kt
+++ infrastructure/monitoring/monitoring-server/src/test/kotlin/at/mocode/infrastructure/monitoring/MonitoringServerApplicationTest.kt
@@ -1,7 +1,6 @@
 package at.mocode.infrastructure.monitoring
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
